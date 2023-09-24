f20e0cfaf0749c8e97a122d6e81f92f129bf60a5
Alan Blanchet <alan@alan-blanchet.fr>
2023-09-24 18:55:51 +0200

    [DEV]Added whitening
    Whitening to reduce pixel covariance

--- src/model/train.py
+++ src/model/train.py
@@ -1,11 +1,14 @@
+import os
 from pathlib import Path
 
 import numpy as np
 import pandas as pd
+import ray
 import torch
 import torch.nn.functional as F
 import torch.optim as optim
 import torchvision.transforms as T
+from ray import air, tune
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader
@@ -15,6 +18,10 @@ import wandb
 from src.data.dataset import StanfordDogsDataset
 from src.model.label import LabelMap
 from src.model.model import StanfordDogsModel
+
+os.environ["RAY_AIR_NEW_OUTPUT"] = "0"
+torch.cuda.init()
+ray.init()
 
 
 def train():
@@ -40,122 +47,154 @@ def train():
 
     train_dataloader = DataLoader(dataset, batch_size=batch_size, sampler=idx_train)
 
-    model = StanfordDogsModel(img_size, len(dataset.label_map))
-    model = torch.compile(model)
-    model = model.to(device)
+    def run(config):
+        objective = 0
 
-    epochs = 10
-    lr = 1e-3
+        model = StanfordDogsModel(img_size, len(dataset.label_map))
+        model = torch.compile(model)
+        model = model.to(device)
 
-    n = 1
-    run_p = log_dir / f"run-{n}"
-    while run_p.exists():
+        n = 1
         run_p = log_dir / f"run-{n}"
-        n += 1
-    run_p.mkdir(exist_ok=True)
+        while run_p.exists():
+            run_p = log_dir / f"run-{n}"
+            n += 1
+        run_p.mkdir(exist_ok=True)
 
-    optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9)
-
-    # start a new wandb run to track this script
-    wandb.init(
-        # set the wandb project where this run will be logged
-        project="OC5",
-        # track hyperparameters and run metadata
-        config={
-            "learning_rate": lr,
-            "architecture": type(model.model).__name__,
-            "dataset": "https://huggingface.co/datasets/Alanox/stanford-dogs",
-            "epochs": epochs,
-        },
-    )
-
-    # =============== Run loop
-    for epoch in trange(epochs, position=0, desc="Epoch", colour="blue", leave=True):
-        losses: list[int] = []
-        y_preds: list[str] = []
-        y_trues: list[str] = []
-
-        # TRAIN
-        for i, datapoints in tqdm(
-            enumerate(train_dataloader, start=1),
-            desc="Training...",
-            position=1,
-            leave=False,
-        ):
-            target = datapoints["target"]
-            image = datapoints["image"]
-            targets = torch.tensor(dataset.label_map[target]).to(device)
-
-            model.zero_grad()
-
-            output = model(image.to(device))
-
-            loss = F.cross_entropy(output, targets)
-            loss.backward()
-
-            # Train log
-            with torch.no_grad():
-                preds = F.softmax(output, dim=1)
-                preds = preds.argmax(dim=1)
-
-                losses.append(loss.item())
-                y_trues.extend(target)
-                y_preds.extend([dataset.label_map[pred.item()] for pred in preds])
-
-            optimizer.step()
-
-        log_metrics(
-            {
-                "train_loss": np.mean(losses),
-                "train_acc": accuracy_score(y_trues, y_preds),
+        # start a new wandb run to track this script
+        run = wandb.init(
+            # set the wandb project where this run will be logged
+            project="OC5",
+            # track hyperparameters and run metadata
+            config={
+                "architecture": type(model.model).__name__,
+                "dataset": "https://huggingface.co/datasets/Alanox/stanford-dogs",
+                **config,
             },
-            epoch,
         )
-        losses = []
-        y_preds = []
-        y_trues = []
+        optimizer = optim.SGD(model.parameters(), lr=config["lr"], momentum=0.9)
+        # =============== Run loop
+        for epoch in trange(
+            config["epochs"],
+            position=0,
+            desc="Epoch",
+            colour="blue",
+            leave=True,
+            disable=not config["log"],
+        ):
+            losses: list[int] = []
+            y_preds: list[str] = []
+            y_trues: list[str] = []
 
-        # EVAL
-        with torch.no_grad():
-            for idx in tqdm(
-                idx_test,
-                desc="Evaluation...",
-                colour="magenta",
+            # TRAIN
+            for i, datapoints in tqdm(
+                enumerate(train_dataloader, start=1),
+                desc="Training...",
                 position=1,
                 leave=False,
+                disable=not config["log"],
             ):
-                datapoint = dataset[idx]
-
-                target = datapoint["target"]
-                image = datapoint["image"].unsqueeze(dim=0)
+                target = datapoints["target"]
+                image = datapoints["image"]
                 targets = torch.tensor(dataset.label_map[target]).to(device)
 
-                output = model(image.to(device)).squeeze(dim=0)
+                model.zero_grad()
+
+                output = model(image.to(device))
+
                 loss = F.cross_entropy(output, targets)
+                loss.backward()
 
-                preds = F.softmax(output, dim=0)
+                # Train log
+                with torch.no_grad():
+                    preds = F.softmax(output, dim=1)
+                    preds = preds.argmax(dim=1)
 
-                # Val log
-                losses.append(loss.to("cpu").item())
-                y_preds.append(dataset.label_map[preds.argmax(dim=0).item()])
-                y_trues.append(target)
+                    losses.append(loss.item())
+                    y_trues.extend(target)
+                    y_preds.extend([dataset.label_map[pred.item()] for pred in preds])
 
-        pd.DataFrame({"true": y_trues, "preds": y_preds}).to_csv(
-            run_p / f"{epoch}-val.csv"
-        )
+                optimizer.step()
 
-        log_metrics(
-            {
-                "val_loss": np.mean(losses),
-                "val_acc": accuracy_score(y_trues, y_preds),
-            },
-            epoch,
-            True,
-        )
+            log_metrics(
+                {
+                    "train_loss": np.mean(losses),
+                    "train_acc": accuracy_score(y_trues, y_preds),
+                },
+                epoch,
+                run,
+            )
+            losses = []
+            y_preds = []
+            y_trues = []
+
+            # EVAL
+            with torch.no_grad():
+                for idx in tqdm(
+                    idx_test,
+                    desc="Evaluation...",
+                    colour="magenta",
+                    position=1,
+                    leave=False,
+                    disable=not config["log"],
+                ):
+                    datapoint = dataset[idx]
+
+                    target = datapoint["target"]
+                    image = datapoint["image"].unsqueeze(dim=0)
+                    targets = torch.tensor(dataset.label_map[target]).to(device)
+
+                    output = model(image.to(device)).squeeze(dim=0)
+                    loss = F.cross_entropy(output, targets)
+
+                    preds = F.softmax(output, dim=0)
+
+                    # Val log
+                    losses.append(loss.to("cpu").item())
+                    y_preds.append(dataset.label_map[preds.argmax(dim=0).item()])
+                    y_trues.append(target)
+
+            pd.DataFrame({"true": y_trues, "preds": y_preds}).to_csv(
+                run_p / f"{epoch}-val.csv"
+            )
+
+            objective = accuracy_score(y_trues, y_preds)
+
+            log_metrics(
+                {
+                    "val_loss": np.mean(losses),
+                    "val_acc": objective,
+                },
+                epoch,
+                run,
+                config["log"],
+            )
+
+        run.finish()
+
+        return objective
+
+    scaler = air.ScalingConfig(
+        {"CPU": 0.2, "GPU": 0.25},
+        num_workers=2,
+        # use_gpu=True,
+        # resources_per_worker={"CPU": 1, "GPU": 0.2},
+    )
+    # run({"epochs": 10, "lr": 1e-3, "log": True})
+    tuner = tune.Tuner(
+        tune.with_resources(
+            tune.with_parameters(run),
+            resources=scaler,
+        ),
+        param_space={"lr": tune.uniform(1e-5, 1e-2), "epochs": 10, "log": False},
+        tune_config=tune.TuneConfig(num_samples=20),
+        run_config=air.RunConfig(),
+    )
+    tuner.fit()
 
 
-def log_metrics(metrics, epoch, val=False):
-    wandb.log(metrics, step=epoch)
+def log_metrics(metrics, epoch, run, val=False):
+    run.log(metrics, step=epoch)
 
     if val:
         tqdm.write(
